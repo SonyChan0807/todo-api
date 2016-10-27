9b2a1d22c7563fbc4315df4038dfc4c67fc3c87e
Gerry Chan <gerry0807@gmail.com>
2016-10-27 19:15:38 +0800

    Add Underscore

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var _ = require('underscore');
 // var todos = [{
 // 	id: 1,
 // 	description: 'Take out the trash',
@@ -34,32 +35,35 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId}); // replace forEach
 	
-	todos.forEach(function (todo) {
-		if (todo.id === todoId) {
-			matchedTodo = todo;
-		}
-	});
+	// todos.forEach(function (todo) {
+	// 	if (todo.id === todoId) {
+	// 		matchedTodo = todo;
+	// 	}
+	// });
 	
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}	
-
-	//res.send('Asking for todo with id of ' + req.params.id);
 });
 
 //POST /todos/:id
 app.post('/todos', function(req, res) {
-	var body = req.body;
-	body.id = todoNextId++;  // todoNextId +1
-	//todoNextId++ ;
+	var body = _.pick(req.body, 'description', 'completed');
+	
+	if (_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
+	body.id = todoNextId++;  // equal to todoNextId++ ;
 	todos.push(body);
 	res.json(body);
 });
-
 
 app.listen(PORT, function () {
 	console.log('Express listening on port' + PORT + '!');
